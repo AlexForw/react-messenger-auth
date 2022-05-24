12832d2c4ced6bfb4271dbb80d41c64e956536e9
AlexForw <lmityushkin@mail.ru>
2022-05-24 23:12:27 +0300

    feat: add pages and router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { dataBase } from "./firebase-config";
+
 function App() {
+  const hey = doc(dataBase, 'whitelist', 3)
+  const add = onSnapshot(hey, (elem) => {
+    if(elem.exists()){
+      console.log(elem)
+    }else{
+      console.log('none');
+    }
+  })
+  console.log(dataBase);
   return (
     <div className="App">
-      
+      hey
     </div>
   );
 }
